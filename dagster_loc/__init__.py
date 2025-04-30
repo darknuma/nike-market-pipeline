0e0b16592443a851c3f7ad5b9944ad9cb09d320c
Emmanuel Aminu <aminuemmazon@gmail.com>
2025-04-30 09:51:57 +0100

    defintions solved

---
+++ dagster_loc/__init__.py
@@ -0,0 +1,1 @@
+# This file makes the dagster directory a Python package 
---
+++ dagster_loc/repository.py
@@ -0,0 +1,4 @@
+from .pipeline.definitions import nike_data_repo
+
+# Re-export the repository
+__all__ = ["nike_data_repo"]
